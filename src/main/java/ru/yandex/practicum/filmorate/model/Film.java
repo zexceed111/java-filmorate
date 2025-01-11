4f8536d9658cd7fe27e5e36211d599ed3307f130
Kazurov Kostya <kazurovzxc@mail.ru>
2025-01-11 05:45:22 +0200

    заполнить данными

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,20 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 /**
  * Film.
  */
+
 @Getter
 @Setter
+@Data
 public class Film {
+    private long id;
+    private String name;
+    private String description;
+    private int releaseDate;
+    private int duration;
 }

f26ea50ef79225edfb0f4576645d770be82921f9
Kazurov Kostya <kazurovzxc@mail.ru>
2025-01-11 19:18:58 +0200

    Добавиьт валидацию и логи

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,75 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private Map<Long, Film> films = new HashMap<>();
+
+    private long getNextId() {
+        long currentMaxId = films.keySet().stream().mapToLong(id -> id).max().orElse(0);
+        return ++currentMaxId;
+    }
+
+    @PostMapping
+    public ResponseEntity<Film> addFilm(@RequestBody Film film) {
+        try {
+            if (film.getName() == null || film.getName().isBlank()) {
+                log.error("Валидация не пройдена: название фильма не может быть пустым.");
+                throw new ValidationException("название фильма не может быть пустым.");
+            }
+            if (film.getDescription() != null && film.getDescription().length() > 200) {
+                log.error("Валидация не пройдена: описание фильма не может превышать 200 символов.");
+                throw new ValidationException("максимальная длина описания — 200 символов");
+            }
+            if (film.getDuration() <= 0) {
+                log.error("Валидация не пройдена: продолжительность фильма должна быть положительным числом.");
+                throw new ValidationException("Продолжительность фильма должна быть положительным числом.");
+            }
+
+            LocalDate releaseDate = (LocalDate) film.getReleaseDate();
+            LocalDate minReleaseDate = LocalDate.of(1895, 12, 28);
+            if (releaseDate == null || releaseDate.isBefore(minReleaseDate)) {
+                log.error("Валидация не пройдена: дата релиза не может быть раньше 28 декабря 1895 года.");
+                throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года.");
+            }
+            film.setId(getNextId());
+            films.put(film.getId(), film);
+            log.info("Фильм добавлен: {}", film);
+            return new ResponseEntity<>(film, HttpStatus.CREATED);
+        } catch (ValidationException e) {
+            log.error("Ошибка при добавлении фильма: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Film> updateFilm(@PathVariable Long id, @RequestBody Film film) {
+        if (!films.containsKey(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        film.setId(id);
+        films.put(id, film);
+        log.info("Фильм обновлен {}", films);
+        return new ResponseEntity<>(film, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Film>> getAllFilms() {
+        return new ResponseEntity<>(films.values(), HttpStatus.OK);
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,101 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    private Map<Long, User> users = new HashMap<>();
+
+    private long getNextId() {
+        long currentMaxId = users.keySet().stream().mapToLong(id -> id).max().orElse(0);
+        return ++currentMaxId;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        log.info("Запрос на создание пользователя: {}", user);
+        try {
+            if (user.getEmail() == null || !user.getEmail().contains("@")) {
+                log.error("Валидация не пройдена: Электронная почта не может быть пустой и должна содержать символ '@'.");
+                throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ '@'.");
+            }
+
+            if (user.getLogin() == null || user.getLogin().trim().isEmpty() || user.getLogin().contains(" ")) {
+                log.error("Валидация ен пройдена: Логин не может быть пустым и не может содержать пробелы.");
+                throw new ValidationException("Логин не может быть пустым и не может содержать пробелы.");
+            }
+
+            //тут я не понимаю как добавить обработку исключения, если дата рождения будет в будущем
+            // у меня валидация past стоит и когда делаю проверку ругается, что тип не тот(принимает только true и false)
+        /*
+
+        if (user.getBirthDate() == null || user.getBirthDate().after(new Date())) {
+        throw new ValidationException("Дата рождения не может быть в будущем.");
+        }
+
+         */
+
+            if (user.getName() == null || user.getName().isBlank()) {
+                user.setName(user.getLogin());
+                log.info("Имя пользователя {}", user.getName());
+            }
+            user.setId(getNextId());
+            log.info("Пользователь добавлен: {}", user);
+            users.put(user.getId(), user);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        } catch (ValidationException e) {
+            log.error("Ошибка при добавлении пользователся: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        log.info("Запрос на обновление пользователя с ID {}: {}", id, updatedUser);
+        if (!users.containsKey(id)) {
+            log.warn("Пользователь с ID {} не найден для обновления", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        updatedUser.setId(id);
+        users.put(id, updatedUser);
+        log.info("Пользователь успешно обновлен: {}", updatedUser);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        log.info("Запрос на получение пользователя с ID: {}", id);
+        User user = users.get(id);
+        if (user == null) {
+            log.warn("Пользователь с ID {} не найден", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        log.info("Пользователь найден: {}", user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        log.info("Запрос на удаление пользователя с ID: {}", id);
+        if (!users.containsKey(id)) {
+            log.warn("Пользователь с ID {} не найден для удаления", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        users.remove(id);
+        log.info("Пользователь с ID {} успешно удален", id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }

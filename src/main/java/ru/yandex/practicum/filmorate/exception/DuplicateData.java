2f8cf5643f4496db8398fb5a37f6be4b0c354d5d
Kazurov Kostya <kazurovzxc@mail.ru>
2025-01-13 20:40:17 +0200

    проверяемое исключение :)

--- src/main/java/ru/yandex/practicum/filmorate/exception/DuplicateData.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/DuplicateData.java
@@ -1,7 +1,8 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class DuplicateData extends ValidationException {
+public class DuplicateData extends RuntimeException {
+
     public DuplicateData(String message, Object objForBody) {
-        super(message, objForBody);
+        super(message, (Throwable) objForBody);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.exception;
 import lombok.Getter;
 
 @Getter
-public class ValidationException extends RuntimeException {
+public class ValidationException extends Exception {
     Object objForBody;
 
     public ValidationException(String message, Object objForBody) {

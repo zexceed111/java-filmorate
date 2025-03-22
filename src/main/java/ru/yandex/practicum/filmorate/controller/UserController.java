03f21242294301d989528985f68c3ddc501ec3a7
Kazurov Kostya <kazurovzxc@mail.ru>
2025-03-22 16:02:33 +0200

    ТЗ-12.8

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -46,22 +46,24 @@ public class UserController {
 
     @PutMapping("/{id}/friends/{friendId}")
     @ResponseStatus(HttpStatus.OK)
-    public List<UserDto> addFriends(@PathVariable @Positive(message = "Users Id must be positive") long id, @PathVariable @Positive(message = "Users Id must be positive") long friendId) {
+    public List<UserDto> addFriends(@PathVariable @Positive(message = "Users Id must be positive") long id,
+                                    @PathVariable @Positive(message = "Users Id must be positive") long friendId) {
         log.info("\nMaking {} as friend {}", id, friendId);
         if (id == friendId) {
             log.warn("\nNot added friends {} and {} because identifiers are equal", id, friendId);
-            throw new ValidationException("Friends are not added.");
+            throw new ValidationException("Friends are not added.", "Identifiers have not be equal.");
         }
         return userService.makeNewFriendsPair(id, friendId);
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
     @ResponseStatus(HttpStatus.OK)
-    public List<UserDto> killFriendship(@PathVariable @Positive(message = "Users Id must be positive") long id, @PathVariable @Positive(message = "Users Id must be positive") long friendId) {
+    public List<UserDto> killFriendship(@PathVariable @Positive(message = "Users Id must be positive") long id,
+                                        @PathVariable @Positive(message = "Users Id must be positive") long friendId) {
         log.info("\nDelete {} as friend {}", id, friendId);
         if (id == friendId) {
             log.warn("\nNot deleted friendship between {} and {} because identifiers are equal", id, friendId);
-            throw new ValidationException("Friends are not deleted.");
+            throw new ValidationException("Friends are not deleted.", "Identifiers have not be equal.");
         }
         return userService.deleteFromFriends(id, friendId);
     }
@@ -75,11 +77,12 @@ public class UserController {
 
     @GetMapping("/{id}/friends/common/{otherId}")
     @ResponseStatus(HttpStatus.OK)
-    public List<UserDto> getCommonFriends(@PathVariable @Positive(message = "Users Id must be positive") long id, @PathVariable @Positive(message = "Users Id must be positive") long otherId) {
+    public List<UserDto> getCommonFriends(@PathVariable @Positive(message = "Users Id must be positive") long id,
+                                          @PathVariable @Positive(message = "Users Id must be positive") long otherId) {
         log.info("\nGetting common friends {} and {}", id, otherId);
         if (id == otherId) {
             log.warn("\nList common friends {} and {} is impossible because identifiers are equal", id, otherId);
-            throw new ValidationException("Friends are not found.");
+            throw new ValidationException("Friends are not found.", "Identifiers have not be equal.");
         }
         return userService.getCommonFriends(id, otherId);
     }
--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -2,10 +2,6 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class NotFoundException extends ValidationException {
     public NotFoundException(String message, Object objForBody) {
-        super(message);
-    }
-
-    public NotFoundException(String message) {
-        super(message);
+        super(message, objForBody);
     }
 }
